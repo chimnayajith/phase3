60e2f226e7cc19c4875e0336f3f8c3f9abd18241
darkkieee <161133805+darkkieee@users.noreply.github.com>
2024-06-04 00:13:35 +0530

    Adminepanel in App.jsx

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
 import Profile from './components/Profile';
+import Navbar from '.components/Navbar';
+import AdminPanel from '.components/AdminPanel;
 function App() {
   return (
     <BrowserRouter>
@@ -8,6 +10,7 @@ function App() {
       {/* <Route path="/" element={<Layout />}> */}
         <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile/>}/>
+      <Route path="/adminpanel" element={<AdminPanel/>}/>
         {/* <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NoPage />} /> */}
       {/* </Route> */}
