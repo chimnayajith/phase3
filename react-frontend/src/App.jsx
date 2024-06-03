e3b7d444f5637c69470a9aed153d8361f00297b3
darkkieee <161133805+darkkieee@users.noreply.github.com>
2024-06-03 23:31:10 +0530

    Update App.jsx

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
-
+import Profile from './components/Profile';
 function App() {
   return (
     <BrowserRouter>
