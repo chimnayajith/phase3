5d975fcde0ff27bb9f54d525564ba846788a19b6
darkkieee <161133805+darkkieee@users.noreply.github.com>
2024-06-04 00:08:20 +0530

    Add files via upload

---
+++ react-frontend/src/components/AdminPanel.jsx
@@ -0,0 +1,45 @@
+// src/components/Attendance.jsx
+import React from 'react';
+import '../styles/AdminPanelStyle.css';
+
+const Attendance = () => {
+  return (
+    <section className="main">
+      <div className="main-top">
+        <h1>Attendance</h1>
+        <FontAwesomeIcon icon={faUserCog} />
+      </div>
+      <section className="attendance">
+        <div className="attendance-list">
+          <h1>Attendance List</h1>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Attendance</th>
+                <th>Batch</th>
+                <th>View</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>23469</td>
+                <td>John Doe</td>
+                <td>69%</td>
+                <td>CSE</td>
+                <td>
+                  <Link to="/profile">
+                    <button>View</button>
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Attendance;
