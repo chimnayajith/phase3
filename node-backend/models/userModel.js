26447d00446b0a635130310267760438b748ee54
Chinmay <chinmayajith30@gmail.com>
2024-06-04 07:22:24 +0530

    backend fix

---
+++ node-backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const db = require("../config/dbConnection");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["student", "faculty"], default: "student" },
+  token: String,
+});
+
+userSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = db.model("Users", userSchema);
